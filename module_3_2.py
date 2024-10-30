f3383c5973132351a340aa8c87ce620c1d4f3b57
OlegEfanov <olegefan@gmail.com>
2024-10-30 15:47:23 +0300

    в работе

--- module_3_2.py
+++ module_3_2.py
@@ -1,5 +1,5 @@
 def  send_email(message, recipient, sender='university.help@gmail.com'):
-    if ("@" and (".com" or ".ru" or ".net")) not in sender: # or ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender):
+    if ("@" and (".com" or ".ru" or ".net")) not in (recipient or sender) or ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender):
         print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
     elif recipient == sender:
         print("Нельзя отправить письмо самому себе!")
@@ -10,6 +10,12 @@ def  send_email(message, recipient, sender='university.help@gmail.com'):
 
 
 send_email("Это сообщение для проверки связи", "vasyok1337@gmail.com")
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.ru')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+
+#Письмо успешно отправлено с адреса university.help@gmail.com на адрес vasyok1337@gmail.com
+#НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса urban.info@gmail.com на адрес urban.fan@mail.ru
+#Невозможно отправить письмо с адреса urban.teacher@mail.uk на адрес urban.student@mail.ru
+#Нельзя отправить письмо самому себе!
