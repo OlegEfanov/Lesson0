535e97e151bfafc0116c376fd21114f2b665723c
OlegEfanov <olegefan@gmail.com>
2024-11-02 13:09:47 +0300

    Рассылка писем

--- module_3_2.py
+++ module_3_2.py
@@ -1,19 +1,23 @@
 def  send_email(message, recipient, sender='university.help@gmail.com'):
-    if ("@" and (".com" or ".ru" or ".net")) not in (recipient or sender) or ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender):
+    def is_valid_email(email):
+        return "@" in email and (email.endswith(".com") or email.endswith(".ru") or email.endswith(".net"))
+
+    if not is_valid_email(sender) or not is_valid_email(recipient):
         print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
+
     elif recipient == sender:
         print("Нельзя отправить письмо самому себе!")
+
     elif sender == "university.help@gmail.com":
         print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
+
     elif sender != "university.help@gmail.com":
         print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
 
-
 send_email("Это сообщение для проверки связи", "vasyok1337@gmail.com")
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.ru')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmai.ru')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
-
 
 #Письмо успешно отправлено с адреса university.help@gmail.com на адрес vasyok1337@gmail.com
 #НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса urban.info@gmail.com на адрес urban.fan@mail.ru
