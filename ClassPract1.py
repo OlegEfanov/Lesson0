16f0d968f972e265800c893389e01bdd24ceda0a
OlegEfanov <olegefan@gmail.com>
2024-11-28 22:47:43 +0300

    Add files via upload

--- ClassPract1.py
+++ ClassPract1.py
@@ -12,10 +12,49 @@ class User:
     """
     def __init__(self, username, password, password_confirm):
         self.username = username
-        if password == password.confirm:
+        if password == password_confirm:
             self.password = password
 
-if __name__ == 'main':
+    def check_password(self,password):
+        # Проверяем длину пароля
+        if len(password) < 8:
+            return False
+        # Проверяем, есть ли в пароле хотя бы одна латинская буква в верхнем регистре
+        if not any(char.isupper() for char in password):
+            return False
+        # Проверяем, есть ли в пароле хотя бы одна латинская буква в нижнем регистре
+        if not any(char.islower() for char in password):
+            return False
+        # Проверяем, есть ли в пароле хотя бы одна цифра
+        if not any(char.isdigit() for char in password):
+            return False
+        # Если все проверки пройдены успешно, возвращаем True
+        return True
+
+    #if check_password(password):
+    #    print("Пароль надёжный")
+    #else:
+    #    print("Пароль ненадёжный")
+
+if __name__ == '__main__':
     database = Database()
-    user = User(input('Введите логин:'), input('Введите пароль:'),input('Повторите пароль:'))
-    database.add_user(user.username, user.password)
+    while True:
+        choice = int(input("Приветствую! Выберите действия: \n1 - Вход\n2 - Регистрация\n"))
+        if choice == 1:
+            login = input('Введите логин:')
+            password = input('Введите пароль:')
+            if login in database.data:
+                if password == database.data[login]:
+                    print(f'Вход выполнен. {login}')
+                    break
+                else:
+                    print('Повторите пароль.')
+            else:
+                print('Пользователь не найдет.')
+        if choice == 2:
+            user = User(input('Введите логин:'), password := input('Введите пароль:'),
+                        password2 := input('Повторите пароль:'))
+            if password != password2:
+                print('Пароли не совподают, по пробуйте еще раз.')
+                continue
+            database.add_user(user.username, user.password)
