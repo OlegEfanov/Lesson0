bc6ea4d8125f5ff30b54f60f34fce2117c6d3262
OlegEfanov <olegefan@gmail.com>
2024-12-19 15:58:25 +0300

    Add files via upload

--- module_7_1.py
+++ module_7_1.py
@@ -1,5 +1,4 @@
-from unicodedata import category
-
+from pprint import pprint
 
 class Product:
 
@@ -9,7 +8,29 @@ class Product:
         self.category = category
 
     def  __str__(self):
-        return f'{self.name},",",{self.weight},",",{self.category}'
+        return f'{self.name}, {self.weight}, {self.category}'
 
 class Shop:
     __file_name = 'products.txt'
+
+    def get_products(self, __file_name):
+        file = open(__file_name, 'r')
+        pprint(file.read())
+        return file.close()
+
+    def add(self, *products, __file_name):
+        self.products = products
+        file = open(__file_name, 'w')
+        
+
+        pass
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
