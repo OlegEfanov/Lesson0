6e028710cb75841ce96e6d3203e282fae943b097
OlegEfanov <olegefan@gmail.com>
2024-12-25 18:20:16 +0300

    Delete 7.py

--- 7.py
+++
@@ -1,4 +0,0 @@
-print('Привет, ' + 'Мир!')
-print('Меня зовут %(name)s, мне %(year)s' % {'name': 'Олег', 'year': 14})
-print('Я учусь в {title} {postfix} {title}'.format(title='Урбан', postfix='University'))
-print(f'{"Urban" * 2} Это лучший университет.')
