2d4c905df832e9fd4b792cdb6731ec8aa8b003f4
OlegEfanov <olegefan@gmail.com>
2024-12-25 16:02:12 +0300

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,4 @@
+print('Привет, ' + 'Мир!')
+print('Меня зовут %(name)s, мне %(year)s' % {'name': 'Олег', 'year': 14})
+print('Я учусь в {title} {postfix} {title}'.format(title='Урбан', postfix='University'))
+print(f'{"Urban" * 2} Это лучший университет.')
